e224140ecc6eb1849aaea67fdb9583fb7208733d
Ivan Yurin <rusbeldoor@gmail.com>
2020-09-11 14:06:09 +0300

    master

--- common/assets/AssetBundle.php
+++ common/assets/AssetBundle.php
@@ -8,7 +8,7 @@ namespace rusbeldoor\yii2General\common\assets;
  */
 class AssetBundle extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@vendor/rusbeldoor/yii2General/common/web';
+    public $sourcePath = '@vendor/rusbeldoor/yii2-general/common/web';
     public $css = ['css/main.css'];
     public $js = [
 		'js/main.js',
--- frontend/assets/AssetBundle.php
+++ frontend/assets/AssetBundle.php
@@ -8,7 +8,7 @@ namespace rusbeldoor\yii2General\frontend\assets;
  */
 class AssetBundle extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@vendor/rusbeldoor/yii2General/frontend/web';
+    public $sourcePath = '@vendor/rusbeldoor/yii2-general/frontend/web';
     public $css = ['css/main.css'];
     public $js = ['js/main.js'];
 }
