2db45565c2de468ac1d6983b977c9053a816cb17
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-11 23:56:46 +0300

    master

--- common/widgets/grid/GridView.php
+++ common/widgets/grid/GridView.php
@@ -8,11 +8,9 @@ namespace rusbeldoor\yii2General\common\widgets\grid;
  */
 class GridView extends \yii\grid\GridView
 {
-	public $pager = [
-		'pager' => [
-            'class' => 'yii\bootstrap4\LinkPager',
-            'firstPageLabel' => 'Первая',
-            'lastPageLabel'  => 'Последняя',
-        ],
-	];
+    public $pager = [
+        'class' => 'yii\bootstrap4\LinkPager',
+        'firstPageLabel' => 'Первая',
+        'lastPageLabel'  => 'Последняя',
+    ];
 }
