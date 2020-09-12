707519af1e862b911a32e609db76f63f78769eaf
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-13 02:10:34 +0300

    master

--- gii/crud/form.php
+++ gii/crud/form.php
@@ -8,7 +8,7 @@ echo $form->field($generator, 'searchModelClass');
 echo $form->field($generator, 'controllerClass');
 // echo $form->field($generator, 'viewPath');
 // echo $form->field($generator, 'baseControllerClass');
-echo $form->hiddenField($generator, 'indexWidgetType', ['value' => 'grid']);
+echo $form->field($generator, 'indexWidgetType', ['value' => 'grid'])->hiddenInput();
 /* echo $form->field($generator, 'indexWidgetType')->dropDownList([
     'grid' => 'GridView',
     'list' => 'ListView',
