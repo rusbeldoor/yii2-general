3e3098d68fa5610251f2047b54ad0d33be114321
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-13 15:44:00 +0300

    master

--- gii/crud/default/views/index.php
+++ gii/crud/default/views/index.php
@@ -34,7 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'tableOptions' => ['class' => 'table table-striped table-hover'],
         'columns' => [
-            ['class' => 'rusbeldoor\yii2General\widgets\CheckboxColumn'],
+            ['class' => 'rusbeldoor\yii2General\widgets\grid\CheckboxColumn'],
 <?php
 foreach ($generator->getColumnNames() as $name) {
     echo "            ";
