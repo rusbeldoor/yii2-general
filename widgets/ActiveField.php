20dc723787b84a8e100af7c3db17e73e69fb9740
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-17 23:30:48 +0300

    master

--- widgets/ActiveField.php
+++ widgets/ActiveField.php
@@ -77,19 +77,33 @@ class ActiveField extends \kartik\form\ActiveField
     public function searchNumberYesNo($options = [])
     { return self::radioButtonsList(['' => 'Не важно', '1' => 'Да', '0' => 'Нет'], $options); }
 
-    public function dateTime()
+    /**
+     * ...
+     *
+     * @param $options array
+     * @return ActiveField
+     */
+    public function dateTimePicker($options)
     {
-        return $this->widget('kartik\daterange\DateRangePicker', [
-            'useWithAddon' => true,
-            'convertFormat' => true,
-            'readonly' => true,
-            'pluginOptions' => [
-                'timePicker' => true,
-                'timePickerIncrement' => 5,
-                'locale' => ['format' => 'H:i d.m.Y'],
-                'singleDatePicker' => true,
-                'showDropdowns' => true
-            ]
-        ]);
+        $this->addon = ['prepend' => ['content' => '<i class="fas fa-calendar-alt"></i>']];
+        return $this->widget('kartik\daterange\DateRangePicker', $options);
     }
+
+    /**
+     * ...
+     *
+     * @return ActiveField
+     */
+    public function dateTime()
+    { return self::dateTimePicker([
+        'convertFormat' => true,
+        'readonly' => true,
+        'pluginOptions' => [
+            'timePicker' => true,
+            'timePicker24Hour' => true,
+            'timePickerIncrement' => 5,
+            'locale' => ['format' => 'H:i d.m.Y'],
+            'singleDatePicker' => true,
+        ]
+    ]); }
 }
