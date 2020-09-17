a1ada34d21e01f60183456bfacee604ad30b737d
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-17 23:43:10 +0300

    master

--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -12,4 +12,10 @@ class ActiveForm extends \kartik\form\ActiveForm
      * @see fieldConfig
      */
     public $fieldClass = 'rusbeldoor\yii2General\widgets\ActiveField';
+
+    /**
+     * @var string form orientation type (for bootstrap styling). Either [[TYPE_VERTICAL]], [[TYPE_HORIZONTAL]], or
+     * [[TYPE_INLINE]]. Defaults to [[TYPE_VERTICAL]].
+     */
+    public $type = self::TYPE_HORIZONTAL;
 }
