57d713299c4da77c90a25baf8de031a2b47ee5d1
Ivan Yurin <rusbeldoor@gmail.com>
2020-09-17 18:53:13 +0300

    master

--- gii/model/default/model.php
+++ gii/model/default/model.php
@@ -82,7 +82,7 @@ class <?= $className ?> extends \rusbeldoor\yii2General\common\models\ActiveReco
         return [
 <?php foreach ($labels as $name => $label) {
     echo "            ";
-    echo ((isset($attributeLabels[$name])) ? "'$name' => " . $generator->generateString($label) : "'" . $name . "' => '" . $label . "'");
+    echo ((isset($attributeLabels[$name])) ? "'$name' => " . $attributeLabels[$name] : "'" . $name . "' => '" . $generator->generateString($label) . "'");
     echo ",\n";
 } ?>
         ];
