291df3d710cc234be407659855f18431cd28136b
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-19 20:15:00 +0300

    master

--- common/assets/AssetBundle.php
+++ common/assets/AssetBundle.php
@@ -21,6 +21,7 @@ class AssetBundle extends \yii\web\AssetBundle
 
     public $depends = [
         'yii\web\JqueryAsset',
+        'yii\bootstrap4\BootstrapAsset',
         'rusbeldoor\yii2General\common\assets\FontawesomeAssetBundle',
     ];
 }
