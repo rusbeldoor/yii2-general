6dcc31283745b86d3b09639249ee661e86e6d69a
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-20 13:08:41 +0300

    master

--- widgets/Select2.php
+++ widgets/Select2.php
@@ -26,7 +26,7 @@ class Select2 extends \kartik\select2\select2
                     [
                         'type'=> 'button',
                         'class' => 'btn btn-light buttonReverseSelect',
-                        'style' => 'margin-left: 5px; float: right;',
+                        'style' => 'width: 46px; margin-left: 5px; float: right;',
                         'data-select-id' => '2',
                         'disabled' => false,
                     ]
@@ -36,21 +36,21 @@ class Select2 extends \kartik\select2\select2
                     [
                         'type'=> 'button',
                         'class' => 'btn btn-light buttonCheckAllSelect',
-                        'style' => 'margin-left: 5px; float: right;',
+                        'style' => 'width: 46px; margin-left: 5px; float: right;',
                         'data-select-id' => '1',
                         'disabled' => false,
                     ]
                 );
         }
 
-        $this->pluginOptions['width'] = 'calc(100% - ' . ($buttonsCount * (42 + 5)) . 'px)';
+        $this->pluginOptions['width'] = 'calc(100% - ' . ($buttonsCount * (46 + 5)) . 'px)';
         $buttonsHtml .=
             Html::button(
                 '<i class="fas fa-wind"></i>',
                 [
                     'type'=> 'button',
                     'class' => 'btn btn-light buttonResetSelect',
-                    'style' => 'margin-left: 5px; float: right;',
+                    'style' => 'width: 46px; margin-left: 5px; float: right;',
                     'data-select-id' => '3',
                     'disabled' => false,
                 ]
