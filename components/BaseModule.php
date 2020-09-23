0d284d6f255b028532bee9823cdbfce47515de82
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-23 23:31:17 +0300

    master

--- components/BaseModule.php
+++ components/BaseModule.php
@@ -2,9 +2,27 @@
 
 namespace rusbeldoor\yii2General\components;
 
+use Yii;
+use yii\helpers\Html;
+
 /**
  * ...
  */
 class BaseModule extends \yii\base\Module
 {
+    /**
+     * Меню
+     *
+     * @return array
+     */
+    public function menu()
+    {
+        $menuItems = [];
+        if (Yii::$app->user->isGuest) {
+            $menuItems[] = ['label' => 'Вход', 'url' => ['/site/login']];
+        } else {
+            $menuItems[] = '<li class="nav-item">' . Html::beginForm(['/site/logout'], 'post') . Html::submitButton('Выход (' . Yii::$app->user->identity->username . ')', ['class' => 'btn nav-link']) . Html::endForm() . '</li>';
+        }
+        return $menuItems;
+    }
 }
