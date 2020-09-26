8f8ebb09481a8c0e89696bdf721f1cf7b9d494e5
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-26 11:39:41 +0300

    master

--- backend/modules/admin/modules/rbac/views/role/_form.php
+++ backend/modules/admin/modules/rbac/views/role/_form.php
@@ -1,7 +1,7 @@
 <?php
 
 use rusbeldoor\yii2General\widgets\AddEditForm;
-use kartik\sortable\Sortable;
+use kartik\sortable\SortableInput;
 
 /* @var $this yii\web\View */
 /* @var $model backend\modules\admin\modules\rbac\models\AuthItem */
@@ -9,31 +9,44 @@ use kartik\sortable\Sortable;
 ?>
 
 <div class="auth-item-form">
-    <?php $form = AddEditForm::begin() ?>
+    <? $form = AddEditForm::begin() ?>
     <?= $form->field($model, 'name') ?>
     <?= $form->field($model, 'description') ?>
-    <?
-    echo Sortable::widget([
-        'connected'=>true,
-        'items'=>[
-            ['content'=>'From Item 1'],
-            ['content'=>'From Item 2'],
-            ['content'=>'From Item 3'],
-            ['content'=>'From Item 4'],
-        ]
-    ]);
-    echo Sortable::widget([
-        'connected'=>true,
-        'itemOptions'=>['class'=>'alert alert-warning'],
-        'items'=>[
-            ['content'=>'To Item 1'],
-            ['content'=>'To Item 2'],
-            ['content'=>'To Item 3'],
-            ['content'=>'To Item 4'],
-        ]
-    ]);
-    echo '<div class="clearfix"></div>';
-    ?>
+    <div class="row">
+        <div class="col-6">
+            <?= SortableInput::widget([
+                'name' => 'kv-conn-1',
+                'items' => [
+                    1 => ['content' => 'Item # 1'],
+                    2 => ['content' => 'Item # 2'],
+                    3 => ['content' => 'Item # 3'],
+                    4 => ['content' => 'Item # 4'],
+                    5 => ['content' => 'Item # 5'],
+                ],
+                'hideInput' => false,
+                'sortableOptions' => ['connected' => true],
+                'options' => ['class' => 'form-control', 'readonly' => true]
+            ]) ?>
+        </div>
+        <div class="col-6">
+            <?= SortableInput::widget([
+                'name' => 'kv-conn-2',
+                'items' => [
+                    10 => ['content' => 'Item # 10'],
+                    20 => ['content' => 'Item # 20'],
+                    30 => ['content' => 'Item # 30'],
+                    40 => ['content' => 'Item # 40'],
+                    50 => ['content' => 'Item # 50'],
+                ],
+                'hideInput' => false,
+                'sortableOptions' => [
+                    'itemOptions' => ['class' => 'alert alert-warning'],
+                    'connected' => true,
+                ],
+                'options' => ['class' => 'form-control', 'readonly' => true]
+            ]) ?>
+        </div>
+    </div>
     <?= $form->buttons($model) ?>
-    <?php AddEditForm::end() ?>
+    <? AddEditForm::end() ?>
 </div>
