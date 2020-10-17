0d31aaf714e7be175d50f36f9fd15f4a99381368
Ivan Yurin <i.yurin@czi-grif.ru>
2020-10-17 15:14:16 +0300

    master

--- modules/subscriptions/views/default/subscriptions.php
+++ modules/subscriptions/views/default/subscriptions.php
@@ -2,34 +2,42 @@
 /* @var $this yii\web\View */
 /* @var $result array */
 
-function write($elems) {
+/**
+ * Вывод элементов
+ *
+ * @param $elems array
+ */
+function writeElems($elems) {
     foreach ($elems as $keyAlias => $elem) {
         foreach ($elem['channels'] as $chanelAlias => $channelName) {
             ?><div class="card" style="float: left; margin: 0 10px 10px 0;">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $elem['name'] ?></h5>
-                    <p class="card-text"><?= $channelName ?></p>
-                    <a href="#" class="btn btn-primary">Отписаться</a>
-                </div>
+            <div class="card-body">
+                <h5 class="card-title"><?= $elem['name'] ?></h5>
+                <p class="card-text"><?= $channelName ?></p>
+                <a href="#" class="btn btn-primary">Отписаться</a>
+            </div>
             </div><?
         }
-        if (count($elem['childKeys'])) { write($elem['childKeys']); }
+        if (count($elem['childKeys'])) { writeElems($elem['childKeys']); }
     }
 }
 
-$this->registerJs('$(document).ready(function () {
+$this->registerJs(
+'$(document).ready(function () {
     $(\'.btn.btn-primary\').click(function () {
         confirmDialog({
-            text: \'Вы уверены, что хотите выбрать все шаблоны абонементов?\',
+            text: \'Вы уверены, что хотите отписаться?\',
             confirmCallback: () => {
                 alert(123);
             }
         });
     });
-});');
+});'
+);
 ?>
+
 <h1>Подписки на рассылки</h1>
 <?
-if (count($result)) { echo '<div class="row">'; write($result); echo '</div>'; }
+if (count($result)) { writeElems($result); }
 else { echo 'У Вас нет ни одной подписки на рассылки.'; }
 ?>
