65da5fb5fba0d65f9fd32937da89abb403a3e0b0
Ivan Yurin <i.yurin@czi-grif.ru>
2020-10-24 00:23:24 +0300

    master

--- modules/subscriptions/views/default/subscriptions.php
+++ modules/subscriptions/views/default/subscriptions.php
@@ -20,18 +20,19 @@ function writeElems($elems, $userId) {
             <div class="card-body">
                 <h5 class="card-title"><?= $key['name'] ?></h5><?
                 foreach ($key['channels'] as $channel) {
-                    $iconClass = null;
+                    $iconClass = (($channel['active']) ? 'far fa-frown' : 'far fa-smile');
+                    $channelIconClass = null;
                     switch ($channel['alias']) {
-                        case 'email': $iconClass = 'fas fa-envelope'; break;
-                        case 'sms': $iconClass = 'fas fa-sms'; break;
-                        case 'vkontakte': $iconClass = 'fab fa-vk'; break;
-                        case 'odnoklassniki': $iconClass = 'fab fa-odnoklassniki'; break;
-                        case 'facebook': $iconClass = 'fab fa-facebook-f'; break;
-                        case 'instagram': $iconClass = 'fab fa-instagram'; break;
-                        case 'whatsapp': $iconClass = 'fab fa-whatsapp'; break;
-                        case 'viber': $iconClass = 'fab fa-viber'; break;
-                        case 'telegram': $iconClass = 'fab fa-telegram-plane'; break;
-                        default: $iconClass = (($channel['active']) ? 'far fa-frown' : 'far fa-smile');
+                        case 'email': $channelIconClass = 'fas fa-envelope'; break;
+                        case 'sms': $channelIconClass = 'fas fa-sms'; break;
+                        case 'vkontakte': $channelIconClass = 'fab fa-vk'; break;
+                        case 'odnoklassniki': $channelIconClass = 'fab fa-odnoklassniki'; break;
+                        case 'facebook': $channelIconClass = 'fab fa-facebook-f'; break;
+                        case 'instagram': $channelIconClass = 'fab fa-instagram'; break;
+                        case 'whatsapp': $channelIconClass = 'fab fa-whatsapp'; break;
+                        case 'viber': $channelIconClass = 'fab fa-viber'; break;
+                        case 'telegram': $channelIconClass = 'fab fa-telegram-plane'; break;
+                        default:
                     }
                     ?><?= Html::beginForm('/subscriptions/default/change', 'post'); ?>
                         <?= Html::input('hidden', 'userId', $userId) ?>
@@ -40,7 +41,7 @@ function writeElems($elems, $userId) {
                         <?= Html::input('hidden', 'hash', SubscriptionHelper::hash($userId, $key['alias'], $channel['alias'])) ?>
                         <?= Html::input('hidden', 'active', (($channel['active']) ? '0' : '1')) ?>
                         <?= Html::input('hidden', 'redirectUrl', Yii::$app->request->url) ?>
-                        <p><button type="button" class="btn btn-<?= (($channel['active']) ? 'light unsubscribe' : 'primary subscribe') ?> "><?= (($iconClass) ? '<i class="' . $iconClass . '"></i>&nbsp;' : '') ?> <?= (($channel['active']) ? 'Отписаться' : 'Подписаться') ?> (<?= $channel['name'] ?>)</button></p>
+                        <p><button type="button" class="btn btn-<?= (($channel['active']) ? 'light unsubscribe' : 'primary subscribe') ?> "><i class="' . $iconClass . '"></i>&nbsp;<?= (($channel['active']) ? 'Отписаться' : 'Подписаться') ?> (<?= (($channelIconClass) ? '<i class="' . $channelIconClass . '"></i>&nbsp;' : '') ?> <?= $channel['name'] ?>)</button></p>
                     <?= Html::endForm(); ?><?
                 }
             ?></div>
