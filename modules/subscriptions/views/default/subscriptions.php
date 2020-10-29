1f69d73973a1c7828c634842ca9fe7c3285fc271
Ivan Yurin <rusbeldoor@gmail.com>
2020-10-29 18:28:03 +0300

    master

--- modules/subscriptions/views/default/subscriptions.php
+++ modules/subscriptions/views/default/subscriptions.php
@@ -42,17 +42,18 @@ function writeElems($elems, $userId) {
     }
 }
 
-$this->registerJs(
-'$(document).ready(function () {
-    $(\'.unsubscribe\').click(function () {
+$javascript = <<< JS
+$(document).ready(function () {
+    $('.unsubscribe').click(function () {
         confirmDialog({
-            text: \'Вы уверены, что хотите отписаться от "\' + $(this).data(\'key-name\') + \'" (\' + $(this).data(\'channel-name\') + \')? <i class="far fa-frown"></i>\',
-            confirmCallback: () => { $(this).closest(\'form\').submit(); }
+            text: 'Вы уверены, что хотите отписаться от "' + $(this).data('key-name') + '" (' + $(this).data('channel-name') + ')? <i class="far fa-frown"></i>',
+            confirmCallback: () => { $(this).closest('form').submit(); }
         });
     });
-    $(\'.subscribe\').click(function () { $(this).closest(\'form\').submit(); });
-});'
-);
+    $('.subscribe').click(function () { $(this).closest('form').submit(); });
+});
+JS;
+$this->registerJs($javascript);
 ?>
 
 <?
