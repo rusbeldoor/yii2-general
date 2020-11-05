40e45b0c556d8f929577d05195be6802a48ba986
Ivan Yurin <i.yurin@czi-grif.ru>
2020-11-05 20:02:54 +0300

    master

--- console/migrations/m200102_000000_rusbeldoor_yii2General_user.php
+++ console/migrations/m200102_000000_rusbeldoor_yii2General_user.php
@@ -17,13 +17,13 @@ class m200102_000000_rusbeldoor_yii2General_user extends Migration
             'id' => $this->primaryKey(11)->unsigned(),
             'username' => $this->string(32)->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string(96)->notNull(),
-            'password_reset_token' => $this->string(96)->unique(),
-            'email' => $this->string(128)->notNull()->unique(),
+            'password_hash' => $this->string(64)->notNull(),
+            'password_reset_token' => $this->string(48)->unique(),
+            'email' => $this->string(96)->notNull()->unique(),
             'status' => $this->tinyInteger(1)->unsigned()->notNull()->defaultValue(10),
             'created_at' => $this->integer(11)->notNull(),
             'updated_at' => $this->integer(11)->notNull(),
-            'verification_token' => $this->string(96)->defaultValue(null),
+            'verification_token' => $this->string(48)->defaultValue(null),
         ]);
     }
 
--- console/migrations/m200930_000000_rusbeldoor_yii2General_yandex_direct.php
+++ console/migrations/m200930_000000_rusbeldoor_yii2General_yandex_direct.php
@@ -15,17 +15,17 @@ class m200930_000000_rusbeldoor_yii2General_yandex_direct extends Migration
         // Таблица компаний Яндекс.Директ
         $this->createTable('yandex_direct_campaign', [
             'id' => $this->primaryKey(11)->unsigned(),
-            'name' => $this->varchar(255)->notNull(),
-            'status' => $this->varchar(16)->notNull(),
-            'state' => $this->varchar(16)->notNull(),
+            'name' => $this->string(255)->notNull(),
+            'status' => $this->string(16)->notNull(),
+            'state' => $this->string(16)->notNull(),
         ]);
 
         // Таблица групп объявлений Яндекс.Директ
         $this->createTable('user_subscription', [
             'id' => $this->primaryKey(11)->unsigned(),
             'campaign_id' => $this->integer(11)->unsigned()->notNull(),
-            'name' => $this->varchar(255)->notNull(),
-            'status' => $this->varchar(16)->notNull(),
+            'name' => $this->string(255)->notNull(),
+            'status' => $this->string(16)->notNull(),
         ]);
         $this->addForeignKey('fk-yandex_direct_adgroup-campaign_id', 'yandex_direct_adgroup', 'campaign_id', 'yandex_direct_campaign', 'id');
 
@@ -34,9 +34,9 @@ class m200930_000000_rusbeldoor_yii2General_yandex_direct extends Migration
             'id' => $this->primaryKey(11)->unsigned(),
             'campaign_id' => $this->integer(11)->unsigned()->notNull(),
             'adgroup_id' => $this->integer(11)->unsigned()->notNull(),
-            'title' => $this->varchar(255)->notNull(),
-            'status' => $this->varchar(16)->notNull(),
-            'state' => $this->varchar(16)->notNull(),
+            'title' => $this->string(255)->notNull(),
+            'status' => $this->string(16)->notNull(),
+            'state' => $this->string(16)->notNull(),
         ]);
         $this->addForeignKey('fk-yandex_direct_ad-campaign_id', 'yandex_direct_ad', 'campaign_id', 'yandex_direct_campaign', 'id');
         $this->addForeignKey('fk-yandex_direct_ad-adgroup_id', 'yandex_direct_ad', 'adgroup_id', 'yandex_direct_adgroup', 'id');
