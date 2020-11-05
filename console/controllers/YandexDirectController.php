1c0234d9aeadb6f56f5125f5c3b11025b1065b0e
Ivan Yurin <i.yurin@czi-grif.ru>
2020-11-06 00:26:19 +0300

    master

--- console/controllers/YandexDirectController.php
+++ console/controllers/YandexDirectController.php
@@ -2,6 +2,9 @@
 namespace rusbeldoor\yii2General\console\controllers;
 
 use rusbeldoor\yii2General\helpers\YandexDirectAPIHelper;
+use rusbeldoor\yii2General\models\YandexDirectAd;
+use rusbeldoor\yii2General\models\YandexDirectAdgroup;
+use rusbeldoor\yii2General\models\YandexDirectCampaign;
 
 /**
  * Контроллер
@@ -13,9 +16,58 @@ class YandexDirectController extends \rusbeldoor\yii2General\components\CronCont
      */
     public function actionIndex()
     {
-        $campaings = YandexDirectAPIHelper::getCampaigns();
-        foreach ($campaings as $campaing) {
-            print_r($campaing);
+        $apiCampaingsIds = [];
+        $apiCampaings = YandexDirectAPIHelper::getCampaigns();
+        foreach ($apiCampaings as $apiCampaing) {
+            $apiCampaingsIds[] = $apiCampaing->Id;
+
+            $campaing = YandexDirectCampaign::find($apiCampaing->Id);
+            if (!$campaing) {
+                $campaing = new YandexDirectCampaign();
+                $campaing->id = $apiCampaing->Id;
+            }
+
+            $campaing->name = $apiCampaing->Name;
+            $campaing->status = $apiCampaing->Status;
+            $campaing->state = $apiCampaing->State;
+            $campaing->save();
+        }
+
+        $apiAdgroupsIds = [];
+        $apiAdgroups = YandexDirectApiHelper::getAdgroups(['CamaignIds' => $apiCampaingsIds]);
+        foreach ($apiAdgroups as $apiAdgroup) {
+            $apiAdgroupsIds[] = $apiAdgroup->Id;
+
+            $adgroup = YandexDirectAdgroup::find($apiAdgroup->Id);
+            if (!$adgroup) {
+                $adgroup = new YandexDirectAdgroup();
+                $adgroup->id = $apiAdgroup->Id;
+                $adgroup->campaign_id = $apiAdgroup->CampaignId;
+            }
+
+            $adgroup->name = $apiAdgroup->Name;
+            $adgroup->status = $apiAdgroup->Status;
+            $adgroup->save();
+        }
+
+        $apiAdsIds = [];
+        $apiAds = YandexDirectApiHelper::getAds(['CamaignIds' => $apiAdgroupsIds]);
+        print_r($apiAds);
+        foreach ($apiAds as $apiAd) {
+            $apiAdsIds[] = $apiAd->Id;
+
+            $ad = YandexDirectAd::find($apiAd->Id);
+            if (!$ad) {
+                $ad = new YandexDirectAd();
+                $ad->id = $apiAd->Id;
+                $ad->campaign_id = $apiAd->CampaignId;
+                $ad->adgroup_id = $apiAd->AdGroupId;
+            }
+
+            $ad->title = $apiAd->title;
+            $ad->status = $apiAd->Status;
+            $ad->state = $apiAd->State;
+            $ad->save();
         }
     }
 }
