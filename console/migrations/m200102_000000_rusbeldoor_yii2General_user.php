66a51d590036c5a03cd9648cf92224eb4fd5a503
Ivan Yurin <i.yurin@czi-grif.ru>
2020-11-05 19:35:01 +0300

    master

--- console/migrations/m200102_000000_rusbeldoor_yii2General_user.php
+++ console/migrations/m200102_000000_rusbeldoor_yii2General_user.php
@@ -17,13 +17,13 @@ class m200102_000000_rusbeldoor_yii2General_user extends Migration
             'id' => $this->primaryKey(11)->unsigned(),
             'username' => $this->string(32)->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string()->notNull(),
-            'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull()->unique(),
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
-            'verification_token' => $this->string()->defaultValue(null),
+            'password_hash' => $this->string(96)->notNull(),
+            'password_reset_token' => $this->string(96)->unique(),
+            'email' => $this->string(128)->notNull()->unique(),
+            'status' => $this->tinyInteger(1)->unsigned()->notNull()->defaultValue(10),
+            'created_at' => $this->integer(11)->notNull(),
+            'updated_at' => $this->integer(11)->notNull(),
+            'verification_token' => $this->string(96)->defaultValue(null),
         ]);
     }
 
