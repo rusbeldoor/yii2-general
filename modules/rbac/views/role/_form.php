47fc75f1512f2d93f37cfbbaab99e1965cb76e7d
Ivan Yurin <rusbeldoor@gmail.com>
2020-11-27 16:37:49 +0300

    master

--- modules/rbac/views/role/_form.php
+++ modules/rbac/views/role/_form.php
@@ -13,7 +13,7 @@ use kartik\sortinput\SortableInput;
 
 <div class="auth-item-form">
     <? $form = AddEditForm::begin() ?>
-    <?= $form->field($model, 'name')->textInput(['maxlength' => 96]) ?>
+    <?= $form->field($model, 'name')->textInput(['maxlength' => 96, 'readonly' => !$model->isNewRecord()]) ?>
     <?= $form->field($model, 'description')->textInput(['maxlength' => 192]) ?>
     <div class="form-group row">
         <label class="col-form-label col-md-2">Роли</label>
