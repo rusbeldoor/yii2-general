79b0efffdb24ec18d056ab951d5171086a7a4f40
Ivan Yurin <rusbeldoor@gmail.com>
2021-05-29 13:14:10 +0300

    master

--- modules/cron/controllers/DefaultController.php
+++ modules/cron/controllers/DefaultController.php
@@ -7,7 +7,7 @@ use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\data\ActiveDataProvider;
 use rusbeldoor\yii2General\models\Cron;
-use rusbeldoor\yii2General\modules\cron\models\PlatformSearch;
+use rusbeldoor\yii2General\modules\cron\models\CronSearch;
 use rusbeldoor\yii2General\modules\cron\models\CronLogSearch;
 use rusbeldoor\yii2General\helpers\AppHelper;
 
