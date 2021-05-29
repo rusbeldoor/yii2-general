ff2cb007f529d19c76bc932600cd7f95a7a2d6ee
Ivan Yurin <rusbeldoor@gmail.com>
2021-05-29 14:35:27 +0300

    master

--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -16,15 +16,4 @@ class ActiveForm extends \yii\widgets\ActiveForm
     public $fieldClass = 'rusbeldoor\yii2General\widgets\ActiveField';
 
     public $type = self::TYPE_HORIZONTAL;
-
-    /**
-     * Initializes the widget.
-     * This renders the form open tag.
-     */
-    public function init()
-    {
-        $css = ["form-{$this->type}"];
-        Html::addCssClass($this->options, $css);
-        parent::init();
-    }
 }
