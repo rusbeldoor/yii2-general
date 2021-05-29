45b06b9cca6545797efede6258cc4aa73640bef0
Ivan Yurin <rusbeldoor@gmail.com>
2021-05-29 14:17:01 +0300

    master

--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -8,15 +8,10 @@ namespace rusbeldoor\yii2General\widgets;
 //class ActiveForm extends \kartik\form\ActiveForm
 class ActiveForm extends \yii\widgets\ActiveForm
 {
-    /**
-     * @var string the default field class name when calling [[field()]] to create a new field.
-     * @see fieldConfig
-     */
-    public $fieldClass = 'rusbeldoor\yii2General\widgets\ActiveField';
+    const TYPE_VERTICAL = 'vertical';
+    const TYPE_HORIZONTAL = 'horizontal';
 
-    /**
-     * @var string form orientation type (for bootstrap styling). Either [[TYPE_VERTICAL]], [[TYPE_HORIZONTAL]], or
-     * [[TYPE_INLINE]]. Defaults to [[TYPE_VERTICAL]].
-     */
+    public $fieldClass = 'rusbeldoor\yii2General\widgets\ActiveField';
+    
     public $type = self::TYPE_HORIZONTAL;
 }
