3ddde0a6dfd23c0e92ed77cce05bd6acddd1a7a6
Stanislav Apalkov <berpistas@gmail.com>
2021-06-01 15:18:02 +0300

    master

--- modules/subscriptions/views/default/subscriptions.php
+++ modules/subscriptions/views/default/subscriptions.php
@@ -6,16 +6,26 @@
 use yii\bootstrap4\Html;
 use rusbeldoor\yii2General\helpers\UserSubscriptionHelper;
 
-$this->registerCssFile('@web/css/subscription/style');
-$this->registerJs('$(document).ready(function () {
-$(\'.unsubscribe\').click(function () {
-    confirmDialog({
-        text: \'Вы уверены, что хотите отписаться от "\' + $(this).data(\'key-name\') + \'" (\' + $(this).data(\'channel-name\') + \')? <i class="far fa-frown"></i>\',
-        confirmCallback: () => { $(this).closest(\'form\').submit(); }
+$this->registerCss(
+'.card .card-body .channels {
+    margin-bottom: 5px;
+}
+
+.card .card-body .channels form + form {
+    margin-top: 5px;
+}'
+);
+$this->registerJs(
+'$(document).ready(function () {
+    $(\'.unsubscribe\').click(function () {
+        confirmDialog({
+            text: \'Вы уверены, что хотите отписаться от "\' + $(this).data(\'key-name\') + \'" (\' + $(this).data(\'channel-name\') + \')? <i class="far fa-frown"></i>\',
+            confirmCallback: () => { $(this).closest(\'form\').submit(); }
+        });
     });
-});
-$(\'.subscribe\').click(function () { $(this).closest(\'form\').submit(); });
-});');
+    $(\'.subscribe\').click(function () { $(this).closest(\'form\').submit(); });
+});'
+);
 
 if (!count($result)) { echo '<p>У Вас нет указанных подписок на рассылки.</p>'; }
 else {
