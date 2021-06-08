3eee61bdd890f6cea918f2a31d2ffe9c8cd47111
Stanislav Apalkov <berpistas@gmail.com>
2021-06-08 11:52:21 +0300

    master

---
+++ helpers/TranslitHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace rusbeldoor\yii2General\helpers;
+
+class TranslitHelper
+{
+    public static $abc = [
+        'rus' => [
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+        ],
+        'lat' => [
+            'A', 'B', 'V', 'G', 'D', 'E', 'Yo', 'Zh', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Shh', '', '', '', 'E`', 'Yu', 'Ya',
+            'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'shh', '', '', '', 'e`', 'yu', 'ya',
+        ],
+    ];
+
+    /**
+     * ...
+     *
+     * @param string $string
+     * @return string
+     */
+    public static function in($string)
+    { return str_replace(self::$abc['rus'], self::$abc['lat'], $string); }
+
+    /**
+     * ...
+     *
+     * @param string $string
+     * @return string
+     */
+    public static function un($string)
+    { return str_replace(self::$abc['lat'], self::$abc['rus'], $string); }
+}
