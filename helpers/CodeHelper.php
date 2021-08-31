29518eb92b52fe712af3cfd28e0b5234c374ed1d
Rusbeldoor <rusbeldoor@gmail.com>
2021-08-31 22:30:24 +0300

    master

--- helpers/CodeHelper.php
+++ helpers/CodeHelper.php
@@ -20,32 +20,30 @@ class CodeHelper
         'secret' => 'YZRD3XkaQ1Cbce2mnSTopqwW9izEuvUVlstA8JG60B4IfF5H7ghOPxydKLjMNr',
     ];
 
-
     /**
-     * Генерация уникального кода
+     * Генерация уникального кода по массиву
      *
      * @param int $length
-     * @param string $abc
+     * @param array $abcArray
      * @param array $params
      * @return string
      */
-    public static function generate($length, $abc, $params = [])
+    public static function generateByArray($length, $abcArray, $params = [])
     {
         // Параметры
         if (!isset($params['notFirstZero'])) { $params['notFirstZero'] = false; }
         if (!isset($params['notFirstAndLastSpace'])) { $params['notFirstAndLastSpace'] = false; }
 
         // Алфавит
-        $abc = preg_split('/(?<!^)(?!$)/u', $abc);
-        $abc = array_unique($abc);
-        $abcLength = count($abc);
+        $abcArray = array_unique($abcArray);
+        $abcArrayLength = count($abcArray);
 
         $result = '';
 
         // Генерируем необходимое количество символов
         for ($i = 1; $i <= $length; $i++) {
             // Генерируем новый символ
-            $char = $abc[mt_rand(0, ($abcLength - 1))];
+            $char = $abcArray[mt_rand(0, ($abcArrayLength - 1))];
 
             // Если первый символ
             if ($i == 1) {
@@ -82,7 +80,23 @@ class CodeHelper
     }
 
     /**
-     * Генерация уникального кода
+     * Генерация уникального кода по строке
+     *
+     * @param int $length
+     * @param string $abcString
+     * @param array $params
+     * @return string
+     */
+    public static function generateByString($length, $abcString, $params = [])
+    {
+        // Алфавит
+        $abcArray = preg_split('/(?<!^)(?!$)/u', $abcString);
+
+        return self::generateByArray($length, $abcArray, $params);
+    }
+
+    /**
+     * Генерация уникального кода по названиям алфавитов
      *
      * @param int $length
      * @param array $abcNames
@@ -95,7 +109,7 @@ class CodeHelper
         $abc = '';
         foreach ($abcNames as $abcName) { $abc .= self::$abc[$abcName]; }
 
-        return self::generate($length, $abc, $params);
+        return self::generateByString($length, $abc, $params);
     }
 
     /**
