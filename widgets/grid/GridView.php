ba159f4fc3c1e18c0b242c27d9c37f1e30821ae3
Rusbeldoor <rusbeldoor@gmail.com>
2022-07-22 16:09:19 +0300

    master

--- widgets/grid/GridView.php
+++ widgets/grid/GridView.php
@@ -54,44 +54,44 @@ class GridView extends \yii\grid\GridView
         // Реакция на успешное Pjax обновление (переход к таблице)
         // Обработка отправки форм bulkActionForm
         $this->getView()->registerJs(<<< JS
-$(document).ready(function() {
-    function pjaxReload(data, url) {
-        $.pjax.reload({
-            url: url,
-            container: '#{$this->pjaxId}',
-            type: 'POST',
-            data: data
+        $(document).ready(function() {
+            function pjaxReload(data, url) {
+                $.pjax.reload({
+                    url: url,
+                    container: '#{$this->pjaxId}',
+                    type: 'POST',
+                    data: data
+                });
+            }
+            $(document).on('submit', '{$this->searchFormSelector}', function() {
+                pjaxReload($(this).serialize(), window.location.href);
+                return false;
+            });
+            $(document).on('reset', '{$this->searchFormSelector}', function() {
+                setTimeout(function() { pjaxReload($(this).serialize()); }, 1);
+            });
+            $(document).on("pjax:success", "#{$this->pjaxId}",  function(event) { 
+                window.scrollTo({top: $('#{$this->pjaxId}').offset().top, behavior: 'smooth'});
+            });
+            $(document).on('change', '.bulkActionColumnCheckbox', function() {
+                $('.bulkActionFormButton').prop('disabled', (($('#{$this->pjaxId} .grid-view').find('.bulkActionColumnCheckbox:checked').length) ? false : true));
+            });
+            $(document).on('submit', '.bulkActionForm', function() {
+                var keys = $('#{$this->pjaxId} .grid-view').yiiGridView('getSelectedRows');
+                $(this).children('input[name="items"]').val(keys);
+            });
+            $(document).on('click', '.page-link', function(e) {
+                e.preventDefault();
+                pjaxReload($('{$this->searchFormSelector}').serialize(), $(this).attr('href'));
+                return false;
+            });
+            $(document).on('click', '#{$this->pjaxId} .grid-view table thead tr th a', function(e) {
+                e.preventDefault();
+                const href = $(this).attr('href');
+                if (href) { pjaxReload($('{$this->searchFormSelector}').serialize(), href); }
+                return false;
+            });
         });
-    }
-    $(document).on('submit', '{$this->searchFormSelector}', function() {
-        pjaxReload($(this).serialize(), window.location.href);
-        return false;
-    });
-    $(document).on('reset', '{$this->searchFormSelector}', function() {
-        setTimeout(function() { pjaxReload($(this).serialize()); }, 1);
-    });
-    $(document).on("pjax:success", "#{$this->pjaxId}",  function(event) { 
-        window.scrollTo({top: $('#{$this->pjaxId}').offset().top, behavior: 'smooth'});
-    });
-    $(document).on('change', '.bulkActionColumnCheckbox', function() {
-        $('.bulkActionFormButton').prop('disabled', (($('#{$this->pjaxId} .grid-view').find('.bulkActionColumnCheckbox:checked').length) ? false : true));
-    });
-    $(document).on('submit', '.bulkActionForm', function() {
-        var keys = $('#{$this->pjaxId} .grid-view').yiiGridView('getSelectedRows');
-        $(this).children('input[name="items"]').val(keys);
-    });
-    $(document).on('click', '.page-link', function(e) {
-        e.preventDefault();
-        pjaxReload($('{$this->searchFormSelector}').serialize(), $(this).attr('href'));
-        return false;
-    });
-    $(document).on('click', '#{$this->pjaxId} .grid-view table thead tr th a', function(e) {
-        e.preventDefault();
-        const href = $(this).attr('href');
-        if (href) { pjaxReload($('{$this->searchFormSelector}').serialize(), href); }
-        return false;
-    });
-});
         JS);
 
         Pjax::begin([
