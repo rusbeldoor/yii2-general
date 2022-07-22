4870069545bf05be01c16a653a832edb1c618eac
Rusbeldoor <rusbeldoor@gmail.com>
2022-07-22 20:36:58 +0300

    master

--- components/Formatter.php
+++ components/Formatter.php
@@ -17,141 +17,73 @@ class Formatter extends \yii\i18n\Formatter
     { return (($value === null) ? $this->nullDisplay : parent::format($value, $type)); }
 
     /** Ид (идентификатор) */
-    public function asId(int|string $value): string
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return '#' . $value;
-    }
+    public function formatId(int|string $value): string
+    { return '#' . $value; }
+
+    /** Статус */
+    public function formatStatus(string $value): string
+    { return match($value) { 'wait' => 'Ожидает', 'process' => 'Выполняется', 'complete' => 'Выполнено' }; }
+
+    /** Да/Нет */
+    public function formatYesNo(int|string|bool $value): string
+    { return FormatterHelper::yesNo($value); }
+
+    /*** Дата и время ***/
+
+    /** Пример: 18:42 29.11.2020 */
+    public function formatHourMinuteDayMonthYear(int|string $value): string
+    { return DatetimeHelper::formatHourMinuteDayMonthYear($value); }
+
+    /** Пример: 18:42:01 29.11.2020 */
+    public function formatHourMinuteSecondDayMonthYear(int|string $value): string
+    { return DatetimeHelper::formatHourMinuteSecondDayMonthYear($value); }
+
+    /** Пример: 29.11.2020 18:42 */
+    public function formatDayMonthYearHourMinute(int|string $value): string
+    { return DatetimeHelper::formatDayMonthYearHourMinute($value); }
+
+    /** Пример: 29.11.2020 18:42:01 */
+    public function formatDayMonthYearHourMinuteSecond(int|string $value): string
+    { return DatetimeHelper::formatDayMonthYearHourMinuteSecond($value); }
+
+    /** Пример: 18:42 */
+    public function formatHourMinute(int|string $value): string
+    { return DatetimeHelper::formatHourMinute($value); }
+
+    /** Пример: 18:42:01 */
+    public function formatHourMinuteSecond(int|string $value): string
+    { return DatetimeHelper::formatHourMinuteSecond($value); }
+
+    /** Пример: 29.11.2020 */
+    public function formatDayMonthYear($value): string
+    { return DatetimeHelper::formatDayMonthYear($value); }
+
+    /** Пример: 1 520 сек. */
+    public function formatCountSecond(int|string|null $value): string
+    { return DatetimeHelper::formatCountSecond($value); }
 
     /**
-     * 18:42 29.11.2020
-     *
-     * @param mixed $value
-     * @return string
+     * Пример: 1 519 мин. 20 сек.
+     * Пример: 1 519 мин.
+     * Пример: 20 сек.
      */
-    public function asDatetimeHourMinuteDayMonthYear($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatHourMinuteDayMonthYear($value);
-    }
+    public function formatCountMinuteSecond($value): string
+    { return DatetimeHelper::formatCountMinuteSecond($value, ' ', false); }
 
     /**
-     * 18:42:01 29.11.2020
-     *
-     * @param mixed $value
-     * @return string
+     * Пример: 1 519 мин. 20 сек.
+     * Пример: 20 сек.
      */
-    public function asDatetimeHourMinuteSecondDayMonthYear($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatHourMinuteSecondDayMonthYear($value);
-    }
+    public function formatCountHourMinuteSecond(int|string|null $value): string
+    { return DatetimeHelper::formatCountHourMinuteSecond($value, ' ', false); }
 
-    /**
-     * 18:42
-     *
-     * @param mixed $value
-     * @return string
-     */
-    public function asDatetimeHourMinute($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatHourMinute($value);
-    }
+    /*** Ресурсы ***/
 
-    /**
-     * 18:42:01
-     *
-     * @param mixed $value
-     * @return string
-     */
-    public function asDatetimeHourMinuteSecond($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatHourMinuteSecond($value);
-    }
+    /** Рубли с иконкой */
+    public function formatRubleIcon(int|float $value): string
+    { return ResourcesHelper::formatRublesDotIcon($value); }
 
-    /**
-     * 29.11.2020
-     *
-     * @param mixed $value
-     * @return string
-     */
-    public function asDatetimeDayMonthYear($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatDayMonthYear($value);
-    }
-
-    /**
-     * Да / Нет
-     *
-     * @param mixed $value
-     * @return string
-     */
-    public function asYesNo($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        switch ($value) {
-            case 1: case true: case '1': return 'Да';
-            case 0: case false: case '': case '0': return 'Нет';
-            default: return '';
-        }
-    }
-
-    /**
-     * 1 520 сек.
-     *
-     * @param int|string|null $value
-     * @return string
-     */
-    public function asCountSecond($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatCountSecond($value);
-    }
-
-    /**
-     * 1 519 мин. 20 сек.
-     * 1 519 мин.
-     * 20 сек.
-     *
-     * @param int|string|null $value
-     * @return string
-     */
-    public function asCountMinuteSecond($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatCountMinuteSecond($value, ' ', false);
-    }
-
-    /**
-     * 1 519 мин. 20 сек.
-     * 20 сек.
-     *
-     * @param int|string|null $value
-     * @return string
-     */
-    public function asCountHourMinuteSecond($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        return DatetimeHelper::formatCountHourMinuteSecond($value, ' ', false);
-    }
-
-    /**
-     * Статус
-     *
-     * @param mixed $value
-     * @return string
-     */
-    public function asStatus($value)
-    {
-        if ($value === null) { return $this->nullDisplay; }
-        switch ($value) {
-            case 'wait': return 'Ожидает';
-            case 'process': return 'Выполняется';
-            case 'complete': return 'Выполнено';
-            default: return '';
-        }
-    }
+    /** Количество с указанием штук */
+    public function formatCount(int|float $value): string
+    { return ResourcesHelper::formatCount($value, postfixType: 'shortString'); }
 }
