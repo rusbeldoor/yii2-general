09ae3c6673210911656b3704799ce08239be41a1
Rusbeldoor <rusbeldoor@gmail.com>
2022-10-29 12:26:01 +0300

    master

--- helpers/TranslitHelper.php
+++ helpers/TranslitHelper.php
@@ -4,32 +4,22 @@ namespace rusbeldoor\yii2General\helpers;
 
 class TranslitHelper
 {
-    public static $abc = [
-        'cyr' => [
+    public static array $abc = [
+        'cyrillic' => [
             'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
             'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
         ],
-        'lat' => [
+        'latin' => [
             'A', 'B', 'V', 'G', 'D', 'E', 'Yo', 'Zh', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Shh', '', 'Y', '', 'E', 'Yu', 'Ya',
             'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'shh', '', 'y', '', 'e', 'yu', 'ya',
         ],
     ];
 
-    /**
-     * ...
-     *
-     * @param string $string
-     * @return string
-     */
-    public static function in($string)
-    { return str_replace(self::$abc['cyr'], self::$abc['lat'], $string); }
+    /** Транслитерация кирилицы в латиницу */
+    public static function cyrillicToLatin(string $string): string
+    { return str_replace(self::$abc['cyrillic'], self::$abc['latin'], $string); }
 
-    /**
-     * ...
-     *
-     * @param string $string
-     * @return string
-     */
-    public static function un($string)
-    { return str_replace(self::$abc['lat'], self::$abc['cyr'], $string); }
+    /** Транслитерация латиницы в кирилицу */
+    public static function latinToCyrillic(string $string): string
+    { return str_replace(self::$abc['latin'], self::$abc['cyrillic'], $string); }
 }
