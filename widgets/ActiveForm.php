000a3ec25c73653f108d7f65485e0a13bfdfd655
Rusbeldoor <rusbeldoor@gmail.com>
2024-09-01 22:23:03 +0300

    master

--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -6,6 +6,4 @@ namespace rusbeldoor\yii2General\widgets;
 class ActiveForm extends \yii\bootstrap5\ActiveForm
 {
     public $fieldClass = 'rusbeldoor\yii2General\widgets\ActiveField';
-
-    public $layout = self::LAYOUT_FLOATING;
 }
