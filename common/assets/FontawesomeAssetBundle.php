35afbf7d4982a4ae41e62a6a219824e76ed31db9
Rusbeldoor <rusbeldoor@gmail.com>
2024-09-01 23:02:32 +0300

    master

--- common/assets/FontawesomeAssetBundle.php
+++ common/assets/FontawesomeAssetBundle.php
@@ -8,7 +8,7 @@ namespace rusbeldoor\yii2General\common\assets;
  */
 class FontawesomeAssetBundle extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@vendor/fortawesome/font-awesome';
+    public $sourcePath = '@vendor/asmoday74/yii2-fontawesome6';
 
     public $css = [
         'css/all.min.css',
